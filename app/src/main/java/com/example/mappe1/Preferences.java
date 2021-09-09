cf24e67e914d9e1cd7c87008a2a4736f4a0821c0
KapteinKS <sander.toien@outlook.com>
2021-09-09 16:00:54 +0200

    Made preferences page, added some strings

--- app/src/main/java/com/example/mappe1/Preferences.java
+++ app/src/main/java/com/example/mappe1/Preferences.java
@@ -1,6 +1,13 @@
 package com.example.mappe1;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Preferences extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preferences);
+    }
 }
