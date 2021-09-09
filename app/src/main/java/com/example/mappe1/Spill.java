755cd8a99597bb20136bd73bb9ebbc76a358723c
Henrik Stangeland <henrik.b.stangeland@gmail.com>
2021-09-09 13:36:40 +0200

    Started work on game-layout

---
+++ app/src/main/java/com/example/mappe1/Spill.java
@@ -0,0 +1,19 @@
+package com.example.mappe1;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Spill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spill);
+
+        //Button button_0 = (Button)findViewById(R.id.button_0);
+
+
+    }
+}
