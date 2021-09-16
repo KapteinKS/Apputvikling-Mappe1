9ef98f6620d587fe3a8598f39204597fe8bb599f
KapteinKS <sander.toien@outlook.com>
2021-09-16 13:28:41 +0200

    created language chang methods

--- app/src/main/java/com/example/mappe1/Preferences.java
+++ app/src/main/java/com/example/mappe1/Preferences.java
@@ -1,13 +1,37 @@
 package com.example.mappe1;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class Preferences extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preferences);
+    }
+
+    public void norsk(View view) {
+        setLanguage("no");
+        recreate();
+    }
+
+    public void deutch(View view) {
+        setLanguage("de");
+        recreate();
+    }
+
+    public void setLanguage(String landcode){
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration config = res.getConfiguration();
+        config.setLocale(new Locale(landcode));
+        res.updateConfiguration(config, dm);
     }
 }
